3c877fc64868aee30fe6be705f9bcad7ce7f6ec4
Gerardo <you@example.com>
2021-02-21 20:56:28 -0600

    First Commit of the Site. Left Nav at 80%

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+apiKey: "AIzaSyDneMPw_TzL8GoXdOhN8JHVQ3wlO9Q2DrY",
+authDomain: "slack-clone-b2507.firebaseapp.com",
+projectId: "slack-clone-b2507",
+storageBucket: "slack-clone-b2507.appspot.com",
+messagingSenderId: "333573982222",
+appId: "1:333573982222:web:6eff11bdc9b017cf82545f",
+measurementId: "G-SSLSRHFHQJ"
+};
